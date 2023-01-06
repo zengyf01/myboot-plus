7c097c21ede9e2bbd3e11ea33610c67467e54fcf
zengyf01 <zengyf01@mingyuanyun.com>
2023-01-06 18:02:17 +0800

    redisson模块添加看门狗续期配置

--- myboot-plus-starter-redisson/src/main/java/com/myboot/plus/redisson/autoconfiguration/RedissonConfig.java
+++ myboot-plus-starter-redisson/src/main/java/com/myboot/plus/redisson/autoconfiguration/RedissonConfig.java
@@ -8,6 +8,7 @@ import org.redisson.config.SingleServerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,17 +23,24 @@ public class RedissonConfig {
 
     @Value(value = "${spring.redis.host}")
     private String host;
-    @Value(value = "${spring.redis.port}")
+    @Value(value = "${spring.redis.port:6379}")
     private int port;
     @Value(value = "${spring.redis.database:0}")
     private int database;
     @Value(value = "${spring.redis.password:}")
     private String password;
-
+    @Value(value = "${spring.redisson.lockWatchdogTimeout:}")
+    private Long lockWatchdogTimeout;
 
     @Bean(destroyMethod = "shutdown")
     RedissonClient redisson() {
         Config config = new Config();
+
+        if (null != lockWatchdogTimeout) {
+            //指定看门狗续期时间
+            config.setLockWatchdogTimeout(lockWatchdogTimeout);
+        }
+
         if (!host.contains(",")) { //Redis单节点
             SingleServerConfig singleServerConfig = config.useSingleServer();
             //可以用"rediss://"来启用SSL连接
@@ -40,10 +48,13 @@ public class RedissonConfig {
             singleServerConfig.setAddress(address);
             //设置 数据库编号
             singleServerConfig.setDatabase(database);
-            singleServerConfig.setPassword(password);
+            if (!StringUtils.isEmpty(password)) {
+                singleServerConfig.setPassword(password);
+            }
+            singleServerConfig.setPingConnectionInterval(50000);
             //连接池大小:默认值：64
             // singleServerConfig.setConnectionPoolSize()
-        } else { //Redis多节点
+        } else { //Redis集群节点
             ClusterServersConfig clusterServersConfig = config.useClusterServers();
             List<String> nodeAddresses = new ArrayList<>();
             String[] hosts = host.split(",");
@@ -51,6 +62,10 @@ public class RedissonConfig {
                 nodeAddresses.add("redis://"+hosts[i]);
             }
             clusterServersConfig.setNodeAddresses(nodeAddresses);
+            if (!StringUtils.isEmpty(password)) {
+                clusterServersConfig.setPassword(password);
+            }
+
         }
         return Redisson.create(config);
     }
