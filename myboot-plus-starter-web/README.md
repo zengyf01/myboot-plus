e3148a0076a8db131ad04604b3c293df06ea6c4d
zengyf01 <zengyf@mingyuanyun.com>
2025-02-06 23:00:03 +0800

    工程可用稳定版本

---
+++ myboot-plus-starter-web/README.md
@@ -0,0 +1,15 @@
+# 模块说明
+
+## 拓展说明
+ 1、HttpRequesetLogAspect: http请求日志输出
+
+
+
+
+
+
+
+
+
+
+
---
+++ myboot-plus-starter-web/src/main/java/com/myboot/plus/autoconfiguration/aaa.java
@@ -0,0 +1,12 @@
+package com.myboot.plus.autoconfiguration;
+
+import org.springframework.web.bind.annotation.CookieValue;
+
+/**
+ * @Author: zyf
+ * @Date: 2025/2/3 21:53
+ * @Description: TODO
+ */
+public class aaa  {
+
+}
